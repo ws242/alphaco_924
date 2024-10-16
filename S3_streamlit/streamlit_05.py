9ae67d3f10bdab4cc3f247fa2fa1919a33e449f0
campus3 S020 <s020@alpacoedu.onmicrosoft.com>
2024-10-16 16:56:48 +0900

    updated

---
+++ S3_streamlit/streamlit_05.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def cal_sales_revenue(price, total_sales):
+    revenue = price * total_sales
+    return revenue
+
+def main():
+    st.title("여기에서부터 시작")
+    price = st.slider('단가:', 100, 1000, value=500)
+    total_sales = st.slider("판매개수:", 1, 1000, value=500)
+    print(price, total_sales)
+    st.write("단가:", price, "판매개수:", total_sales)
+    
+    if st.button("매출액 계산"):
+        revenue = cal_sales_revenue(price, total_sales)
+        st.write(revenue)
+
+    st.title("Check Box Control")
+    x = np.linspace(0, price, total_sales)
+    y = np.sin(x)
+    z = np.cos(x)
+    show_plot = st.checkbox("시각화 보여주기 ")
+    print(show_plot) # 기본값은 False
+    if show_plot:
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.plot(x, z)
+        st.pyplot(fig)
+
+
+    agree = st.checkbox("I agree")
+    if agree:
+        st.write("Great!")
+
+if __name__ == "__main__":
+    main()
---
+++ S3_streamlit/streamlit_07.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+
+# 데이터 불러오기
+@st.cache_data
+def load_data():
+    df = sns.load_dataset('iris')
+    return df
+
+def main():
+    st.title("SelectBox 사용")
+    iris = load_data()
+    st.markdown("## Raw Data")
+    st.dataframe(iris)
+
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.markdown("## Select")
+    val = st.selectbox("1개의 종을 선택하세요", iris.species.unique())
+    st.write("선택된 species : ", val)
+    # 기존 방식 : iris2 = iris.loc[iris['species'] == val, :].reset_index(drop=true)
+    # st.dataframe(iris2)
+
+    number = st.number_input("Insert a number")
+    st.dataframe(iris.loc[iris['sepal_width'] >= number, :].reset_index(drop=True))
+
+    """
+    st.dataframe(iris.loc[iris['species']==val, :].reset_index(drop=True))
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.markdown("## MultiSelect")
+    cols = st.multiselect("복수의 컬럼을 선택하세요", iris.columns)
+    st.write("선택된 컬럼 : ", cols)
+    st.dataframe(iris.loc[:, cols])
+    """
+
+if __name__ == '__main__':
+    main()
